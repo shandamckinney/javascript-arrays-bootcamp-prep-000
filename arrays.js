5867352431bbae7ba3394506e45d58f2c55efbaa
shandamckinney <mckinney.shanda@gmail.com>
2018-05-08 02:17:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,7 +18,7 @@ function destructivelyAddElementToEndOfArray(array, element){
   return array
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array){
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   array
 }
